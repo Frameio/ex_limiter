54bcc6f0ed1bacd43f0c1e9766fcf054774cbb77
Milton Mazzarri <mmazzarri@adobe.com>
2026-01-14 11:14:13 -0600

    add unit test to verify deletion of expired records

--- test/ex_limiter/storage/pruner_test.exs
+++ test/ex_limiter/storage/pruner_test.exs
@@ -1,14 +1,43 @@
 defmodule ExLimiter.Storage.PG2Shard.PrunerTest do
   use ExLimiter.DataCase, async: false
+
   alias ExLimiter.Storage.PG2Shard.Pruner
 
-  @table_name :pruner_test
   describe "#remove" do
     test "it will remove stale bucket entries" do
-      table = :ets.new(@table_name, [:set, :public, read_concurrency: true, write_concurrency: true])
+      table = :ets.new(:pruner_test, [:set, :public, read_concurrency: true, write_concurrency: true])
 
-      :ets.insert(table, {"bucket", :os.system_time(:millisecond), %ExLimiter.Bucket{}})
+      :ets.insert(table, {"bucket", System.system_time(:millisecond), %ExLimiter.Bucket{}})
       assert Pruner.remove(table, 1) == 1
+      assert :ets.lookup(table, "bucket") == []
+    end
+  end
+
+  describe "#expire" do
+    def expiration_handler([:ex_limiter, :shards, :expirations], %{value: count}, _, %{test_pid: test_pid}) do
+      send(test_pid, {:expired_records, count})
+    end
+
+    test "it will remove expired bucket entries" do
+      table_name = :expired_test
+      table = :ets.new(table_name, [:set, :public, read_concurrency: true, write_concurrency: true])
+
+      # Listen for the expirations events so we can assert on it
+      test_pid = self()
+
+      :telemetry.attach("expiration-test", [:ex_limiter, :shards, :expirations], &__MODULE__.expiration_handler/4, %{
+        test_pid: test_pid
+      })
+
+      on_exit(fn -> :telemetry.detach("expiration-test") end)
+
+      time = System.system_time(:millisecond) - to_timeout(second: 100)
+
+      :ets.insert(table, {"bucket", time, %ExLimiter.Bucket{}})
+
+      assert Pruner.handle_info(:expire, table) == {:noreply, table}
+
+      assert_receive {:expired_records, 1}, to_timeout(second: 5)
       assert :ets.lookup(table, "bucket") == []
     end
   end
